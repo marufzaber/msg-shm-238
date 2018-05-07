506b14889def2c2430e39ceefd5e1b22c8b9dece
Janus Varmarken <varmarken@gmail.com>
2018-05-07 15:54:20 -0700

    Sketch idea for API in msgshm238.h + add note on how to possibly implement blocking receive (to avoid spinning)

---
+++ msgshm238.h
@@ -0,0 +1,27 @@
+struct msgTag;
+
+// Encapsulates the information necessary to send a message to a process.
+typedef struct msgTag {
+    // (Proccess) ID of receiver. TODO: is it more efficient to use pointer to avoid copying (pass-by-value)?
+    int recvId;
+    // ... TODO pointer to location (or copy) of data to be shared.
+    // use of pointer may be more efficient as only one copy will be necessary (from private memory to shared memory segment)
+    // in contrast, pass-by-value will require a copy from local memory to local variable and then to shared memory segment.
+} msg;
+
+// Send message m
+void send(struct msg* m);
+
+// Receive a message from a sender with (process) ID == senderId.
+// Returns a pointer to the message to avoid overhead caused by copying imposed by pass-by-value.
+// TODO: Might actually be necessary to copy msg to local memory for safety purposes (as sender could potentially modify the shared memory segment later on)
+struct msg* recv(int senderId);
+
+//----------------------------------------------------------------------------------
+// Brainstorm -> idea for how to implement blocking recv:
+// Sender (on start up or at first send) locks some memory location identified by its process ID;
+// does not release lock until after it has stored some message in shared memory;
+// then releases lock as a way of notifying receiver that data is ready.
+// ...
+// ... needs more thought
+//----------------------------------------------------------------------------------
--- playground.c
+++ playground.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+void strings() {
+    char str[] = "no notion of 'wrapper' String class/struct like in java;\nstrings are declared as char arrays.\n";
+    printf("%s", str);
+}
+
 int main(void) {
     printf("Hello World!\n");
     if (-42) {
         printf("negative numbers evaluate to true\n");
     }
+    strings();
     return 0;
 }
+
 
