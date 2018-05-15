860d22092a0bbd51c2b89b639964e230dc0544d8
Janus Varmarken <varmarken@gmail.com>
2018-05-14 21:02:38 -0700

    open shared memory segment and add to map (strncpy causes segmentation fault)

--- msgshm238.c
+++ msgshm238.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "msgshm238.h"
 #include <unistd.h>
 #include <sys/types.h>
@@ -8,8 +9,11 @@
 #include <sys/mman.h>
 #include "uthash.h" /* Hash table provided by https://troydhanson.github.io/uthash/ */
 
-#define INT_AS_STR_MAX_CHARS 3*sizeof(int)+2 // Max number of chars needed to convert an int to a string.
-
+/**
+ * Max number of chars needed to convert an int to a string.
+ * See https://stackoverflow.com/a/10536254/1214974
+ */
+#define INT_AS_STR_MAX_CHARS 3*sizeof(int)+2
 
 /*
  * [Note to self]
@@ -28,8 +32,16 @@ pid_t senderId = -42;
  * Type used for constructing a dictionary (hash map), which is in turn used to locate existing shared memory segments.
  */
 typedef struct shm_dictionary_entry {
-    /* For now, the ID is the concatenation of sender and the receiver ID */
-    char id[2*INT_AS_STR_MAX_CHARS];
+    /**
+     * For now, the ID is the concatenation of sender and the receiver ID, hence we must allow room for two ints.
+     * In addition, we must prepend the concatenation of the two IDs with a slash as per the convention specified in the man pages (hence we need the +1 to allow space for an extra char).
+     * See http://man7.org/linux/man-pages/man3/shm_open.3.html
+     */
+    char id[2*INT_AS_STR_MAX_CHARS+1];
+    /**
+     * Poiner to start of shared memory segment.
+     */
+    char *addr;
     /* Used by uthash for internal bookkeeping; must be present. */
     UT_hash_handle hh;
 } shm_dict_entry;
@@ -48,16 +60,52 @@ void send(msg* m) {
     shm_dict_entry *entry;
     // construct the key by concatenating sender and receiver IDs
     char identifier[INT_AS_STR_MAX_CHARS];
-    sprintf(identifier, "%d%d", m->senderId, m->rcvrId);
-    // TODO: need to account for str_len when doing malloc for struct?
+    sprintf(identifier, "/%d%d", m->senderId, m->rcvrId);
     entry = (shm_dict_entry*) malloc(sizeof *entry);
+    // File descriptor for the shared memory segment.
+    int fd;
     // Check if there's already an entry (and thereby already an open shared memory segment).
     HASH_FIND_STR(shm_dict, identifier, entry);
     if(entry == NULL) {
-        printf("no entry found for '%s'\n", identifier);
+        printf("No entry found for '%s'; creating new shared memory segment...\n", identifier);
         /* TODO create shared memory segment and insert in map */
+        fd = shm_open(identifier, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+        if (fd == -1) {
+            printf("Error creating new shared memory segment.\n");
+        } else {
+            printf("Successfully created new shared memory segment with fd=%d\n", fd);
+            /*
+             * New shared memory segments have length 0, so need to size it.
+             *
+             * TODO: need to figure out a way to come up with a reasonable size for the memory segment; for now just size it according to the incoming message.
+             * Essentially the size chosen here will be the size of our message queue/buffer.
+             * We've made it hard for ourselves by choosing a variable size content length of messages (the payload pointer).
+             * Maybe consider making payload a fixed size and null terminate it if the message payload is shorter than what we leave space for.
+             * Ideas?
+             */
+            ftruncate(fd, sizeof(m));
+            // Map shared memory segment into own address space.
+            char *addr;
+            printf("before mmap\n");
+            addr = mmap(NULL, sizeof(m), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+            // Apparently fd is no longer needed. We unlink using the identifer.
+            printf("before close\n");
+            close(fd);
+            // Update map with new entry.
+            printf("before strncpy\n");
+            strncpy(entry->id, identifier, 2*INT_AS_STR_MAX_CHARS+1);
+            printf("after strncpy\n");
+            entry->addr = addr;
+//            *entry = { .id = identifier, .addr = addr };
+//            shm_dict_entry dummy = { .id = identifier, .addr = addr };
+            printf("before adding to hashmap\n");
+            HASH_ADD_STR(shm_dict, id, entry);
+        }
+    } else {
+        /* TODO shared memory segment already already present, use it (access it using 'entry') */
+        printf("Found entry in hash map for '%s'\n", identifier);
     }
-    /* TODO shared memory segment already already present, use it (access it using 'entry') */
+    
 }
 
 msg* constructMsg(char *content, int receiverId) {
