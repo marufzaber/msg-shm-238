47a4e3092cb3dca57373fa6151b35f27853a9898
marufzaber <mzaber@uci.edu>
2018-06-04 20:15:43 -0700

    shared memory - pid added

--- memory_block.c
+++ memory_block.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <unistd.h>
+#include <sys/types.h>
 #inlcude "msgshm238.h"
 
 struct Buffer;
@@ -8,6 +10,8 @@ struct Buffer;
 // Encapsulates the information necessary to send a message to a process.
 typedef struct Buffer {
 	unsigned int mem_count; 
+
+	pid_t pIdOfCurrent
 
     mem_block *head;
     mem_block *tail;
